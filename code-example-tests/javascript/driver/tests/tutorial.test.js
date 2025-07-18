ca27befb30cadf7167d833582254c4c9fd340511
dacharyc <dachary.carey@mongodb.com>
2025-07-18 18:22:24 -0400

    Scaffold workshop files

---
+++ code-example-tests/javascript/driver/tests/tutorial.test.js
@@ -0,0 +1,23 @@
+import { runTutorial } from '../examples/tutorial.js';
+import unorderedArrayOutputMatches from '../utils/outputMatchesExampleOutput.js';
+import { loadSampleData } from '../examples/tutorial-load-sample-data.js';
+import { MongoClient } from 'mongodb';
+
+describe('Aggregation pipeline filter tutorial tests', () => {
+  afterEach(async () => {
+    const uri = process.env.CONNECTION_STRING;
+    const client = new MongoClient(uri);
+    const db = client.db('agg_tutorials_db');
+
+    await db.dropDatabase();
+    await client.close();
+  });
+
+  it('Should return output that matches the expected output', async () => {
+    await loadSampleData();
+    const result = await runTutorial();
+    const outputFilepath = 'tutorial-output.sh';
+    const arraysMatch = unorderedArrayOutputMatches(outputFilepath, result);
+    expect(arraysMatch).toBeTruthy();
+  });
+});
